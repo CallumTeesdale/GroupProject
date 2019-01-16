1c757c9c7b1c832f01efbbd84c0f0d9b5200ce2a
george <george.robertson3@hotmail.co.uk>
2019-01-16 12:42:43 +0000

    initial content commit with some navbar features

---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="styles/stylesheet.css" />
+    <link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
+    <script src="main.js"></script>
+</head>
+<body class= main>
+
+<div class=navbar> 
+
+<div>
+
+<img class=logo src="https://via.placeholder.com/92" alt="">
+<a><h3>Home</h3></a>
+<a><h3>Games</h3></a>
+<a><h3>Consoles</h3></a>
+
+</div>
+
+<div class=searchbar>
+
+<h1>
+
+Search bar
+
+</h1>
+</div>
+
+</div>
+
+</body>
+</html>
