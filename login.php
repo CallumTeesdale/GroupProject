896049b1ec0d7646c3e2848abeed059b9a257d1e
georgerobbo <george.robertson3@hotmail.co.uk>
2019-01-23 19:36:34 +0000

    login page using said framework, has no functionality

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php require 'header.php'
+
+
+ ?>
+
+<div class="title">
+
+  <h1>Login</h1>
+
+</div>
+
+<form class="login-form" action="" method="post">
+
+  <input type="search" placeholder="Username" id="search" name="search" class="search"><br><br><br>
+<input type="search" placeholder="Password" id="search" name="search" class="search"><br><br><br>
+<button type="button" value="login"><i class="fas fa-sign-in-alt"></i> </button>
+
+
+</form>
+
+<?php require 'footer.php'
+
+?>
