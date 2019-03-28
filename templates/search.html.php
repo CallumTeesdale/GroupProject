b28707c6272f469f85048224fd706ad5381b7655
Callum Teesdale <callumjamesteesdale@gmail.com>
2019-03-28 16:47:46 +0000

    Search page changes

--- templates/search.html.php
+++ templates/search.html.php
@@ -1,11 +1,15 @@
 
 <div class="searchWrapper">
   <div class="searchTop">
-    <h1>Search Results for:</h1>
+  <h1>PLATFORM </h1>
+  <h1> SORT: </h1>
+  <h1> PRICE </h1>
+  <h1> RELEASE DATE </h1>
+  <h1> OFFERS </h1>
   </div>
   <div class="searchLeft">
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  </div>
+    <h1>LATEST OFFERS</h1>
+    </div>
   <div class="searchRight">
     <div class="contentSearch">
       <div class="index_games_search">
