7cb251618ec2945d2df5bb2f6b857152538c3f41
Callum Teesdale <callumjamesteesdale@gmail.com>
2019-04-01 22:11:35 +0100

    Prettified

--- templates/search.html.php
+++ templates/search.html.php
@@ -1,45 +1,44 @@
-
 <div class="searchWrapper">
-  <div class="searchTop">
-  <h1>PLATFORM </h1>
-  <h1> SORT: </h1>
-  <h1> PRICE </h1>
-  <h1> RELEASE DATE </h1>
-  <h1> OFFERS </h1>
-  </div>
-  <div class="searchLeft">
-    <h1>LATEST OFFERS</h1>
+    <div class="searchTop">
+        <h1>PLATFORM </h1>
+        <h1> SORT: </h1>
+        <h1> PRICE </h1>
+        <h1> RELEASE DATE </h1>
+        <h1> OFFERS </h1>
     </div>
-  <div class="searchRight">
-    <div class="contentSearch">
-      <div class="index_games_search">
-        <img src="https://via.placeholder.com/200" alt="" class="logo">
-        <h4>Game title here</h4>
-        <p>~300 characters of text</p>
-      </div>
+    <div class="searchLeft">
+        <h1>LATEST OFFERS</h1>
+    </div>
+    <div class="searchRight">
+        <div class="contentSearch">
+            <div class="index_games_search">
+                <img src="https://via.placeholder.com/200" alt="" class="logo">
+                <h4>Game title here</h4>
+                <p>~300 characters of text</p>
+            </div>
 
-      <div class="index_games_search">
-        <img src="https://via.placeholder.com/200" alt="" class="logo">
-        <h4>Game title here</h4>
-        <p>~300 characters of text</p>
-      </div>
+            <div class="index_games_search">
+                <img src="https://via.placeholder.com/200" alt="" class="logo">
+                <h4>Game title here</h4>
+                <p>~300 characters of text</p>
+            </div>
 
-      <div class="index_games_search">
-        <img src="https://via.placeholder.com/200" alt="" class="logo">
-        <h4>Game title here</h4>
-        <p>~300 characters of text</p>
-      </div>
+            <div class="index_games_search">
+                <img src="https://via.placeholder.com/200" alt="" class="logo">
+                <h4>Game title here</h4>
+                <p>~300 characters of text</p>
+            </div>
 
-      <div class="index_games_search">
-        <img src="https://via.placeholder.com/200" alt="" class="logo">
-        <h4>Game title here</h4>
-        <p>~300 characters of text</p>
-      </div>
+            <div class="index_games_search">
+                <img src="https://via.placeholder.com/200" alt="" class="logo">
+                <h4>Game title here</h4>
+                <p>~300 characters of text</p>
+            </div>
 
-      <div class="index_games_search">
-        <img src="https://via.placeholder.com/200" alt="" class="logo">
-        <h4>Game title here</h4>
-        <p>~300 characters of text</p>
-      </div>
-  </div>
-</div>
+            <div class="index_games_search">
+                <img src="https://via.placeholder.com/200" alt="" class="logo">
+                <h4>Game title here</h4>
+                <p>~300 characters of text</p>
+            </div>
+        </div>
+    </div> 
