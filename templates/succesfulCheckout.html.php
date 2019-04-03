679a45e3a865a16600391253d54ef9afe25210c5
Callum Teesdale <callumjamesteesdale@gmail.com>
2019-04-04 00:51:18 +0100

    Succesful checkout display invoice

--- templates/succesfulCheckout.html.php
+++ templates/succesfulCheckout.html.php
@@ -21,6 +21,17 @@ try {
 } catch (Exception $e) {
     die;
 }
+$jsonResult = $result->toJSON();
+$json = json_decode($jsonResult, true);
+$name = $json['payer']['payer_info']['shipping_address']['recipient_name'];
+$address = $json['payer']['payer_info']['shipping_address']['line1'];
+$city = $json['payer']['payer_info']['shipping_address']['city'];
+$postcode = $json['payer']['payer_info']['shipping_address']['postal_code'];
+$total = $json['transactions'][0]['amount']['total'];
+$subtotal = $json['transactions'][0]['amount']['details']['subtotal'];
+$shipping = $json['transactions'][0]['amount']['details']['shipping'];
+
+
 ?>
 <div class="succesfulCheckoutWrapper">
     <div class="succesfulImageWrapper">
@@ -29,13 +40,27 @@ try {
     <div class="succesfulTextWrapper">
         <div class="succesfulText">
             <h1>Invoice</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div class="invAdd">
+            <h1>Shipping Details</h1>
+            <p><strong>Name: </strong><?=$name?></p>
+            <p><strong>Address: </strong><?=$address?></p>
+            <p><strong>City: </strong><?=$city?></p>
+            <p><strong>Postcode: </strong><?=$postcode?></p>
+            </div>
+            <div class="invItems">
+            <h1>Items</h1>
+            <?php 
+            foreach ($json['transactions'][0]['item_list']['items'] as $item): ?>
+            <p><strong>Name: </strong><?=$item['name']?></p>
+            <p><strong>Price: </strong><?=$item['price']?></p>
+            <?php endforeach; ?>
+            <p><strong>Subtotal: </strong><?=$subtotal?></p>
+            <p><strong>Shipping: </strong><?=$shipping?></p>
+            <p><strong>Total: </strong><?=$total?></p>
+            </div>
         </div>
     </div>
 </div> 
 <?php 
-$jsonResult = $result->toJSON();
-$json = json_decode($jsonResult, true);
-$city = $json['payer']['payer_info']['shipping_address']['city'];
-echo $city;
+
 ?>
