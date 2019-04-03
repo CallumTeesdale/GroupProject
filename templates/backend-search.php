b26365b689616e8035d5a802fd2808e18a4d01e1
Callum Teesdale <callumjamesteesdale@gmail.com>
2019-04-03 14:30:13 +0100

    Live search bug fixes

--- templates/backend-search.php
+++ templates/backend-search.php
@@ -2,13 +2,10 @@
     require "../database.php";
     try{
     if(isset($_REQUEST["term"])){
-        // create prepared statement
         $sql = "SELECT * FROM games WHERE game_title LIKE :term";
         $stmt = $pdo->prepare($sql);
         $term = $_REQUEST["term"] . '%';
-        // bind parameters to statement
         $stmt->bindParam(":term", $term);
-        // execute the prepared statement
         $stmt->execute();
         if($stmt->rowCount() > 0){
             while($row = $stmt->fetch()){?>
