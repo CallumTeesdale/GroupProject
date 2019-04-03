2724cb49fb51ba04c6bc5d95ee8c1c5168bbee0b
Callum Teesdale <callumjamesteesdale@gmail.com>
2019-04-03 14:30:27 +0100

    live search bug fix

--- templates/layout.html.php
+++ templates/layout.html.php
@@ -18,22 +18,18 @@
     <script type="text/javascript">
         $(document).ready(function() {
             $('.search input[type="text"]').on("keyup input", function() {
-                /* Get input value on change */
                 var inputVal = $(this).val();
                 var resultDropdown = $(this).siblings(".result");
                 if (inputVal.length) {
                     $.get("/templates/backend-search.php", {
                         term: inputVal
                     }).done(function(data) {
-                        // Display the returned data in browser
                         resultDropdown.html(data);
                     });
                 } else {
                     resultDropdown.empty();
                 }
             });
-
-            // Set search input value on click of result item
             $(document).on("click", ".result p", function() {
                 $(this).parents(".search").find('input[type="text"]').val($(this).text());
                 $(this).parent(".result").empty();
